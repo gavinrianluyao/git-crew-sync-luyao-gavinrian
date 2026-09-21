210c469d0e3f6cca6622ff41723e62b135782399
sky <gavinrian.t@gmail.com>
2026-09-21 12:14:42 +0800

    Added overtime pay for over 8 hours

--- shifts.js
+++ shifts.js
@@ -3,7 +3,15 @@ function isValidShift(hours) {
 }
 
 function calculatePay(hours, rate) {
+  if (hours > 8) {
+    const reg = 8 * rate;
+    const overtime = hours - 8;
+    const overtimePay = reg + (overtime * rate * 1.5);
+    return Math.floor(overtimePay);
+  }
   return Math.floor(hours * rate);
 }
+
+
 
 module.exports = { isValidShift, calculatePay };
